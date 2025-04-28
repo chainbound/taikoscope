17cab7f4c63205b029cf5f47a3ed97271720c766
Håvard Anda Estensen <haavard.ae@gmail.com>
2025-04-28 15:58:40 +0200

    feat: trace instead of print

--- bin/taikoscope/src/main.rs
+++ bin/taikoscope/src/main.rs
@@ -1,31 +1,35 @@
 //! Entrypoint.
 
-use clap::Parser;
-use config::Opts;
 use extractor::Extractor;
 use inserter::ClickhouseClient;
 use tokio_stream::StreamExt;
+
+use clap::Parser;
+use config::Opts;
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     let opts = Opts::parse();
 
-    println!("Initializing ClickHouse client...");
+    tracing_subscriber::fmt().with_max_level(tracing::Level::INFO).init();
+
+    info!("Initializing ClickHouse client...");
     let clickhouse_client = ClickhouseClient::new(&opts.clickhouse_url)?;
     clickhouse_client.init_db().await?;
 
     let rpc_url = "wss://eth.merkle.io";
-    println!("Initializing extractor...");
+    info!("Initializing extractor...");
     let extractor = Extractor::new(rpc_url).await?;
     let mut block_stream = extractor.get_block_stream().await?;
 
-    println!("Processing blocks...");
+    info!("Processing blocks...");
     while let Some(block) = block_stream.next().await {
-        println!("Processing block: {:?}", block.number);
+        info!("Processing block: {:?}", block.number);
 
         // Insert block into ClickHouse
         clickhouse_client.insert_block(&block).await?;
-        println!("Inserted block: {:?}", block.number);
+        info!("Inserted block: {:?}", block.number);
     }
 
     Ok(())
