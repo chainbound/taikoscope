5a201f3a33b0820bdc782909d4b758904a7a7190
Håvard Anda Estensen <haavard.ae@gmail.com>
2025-04-28 11:24:24 +0200

    chore: async main

--- bin/taikoscope/src/main.rs
+++ bin/taikoscope/src/main.rs
@@ -1,8 +1,22 @@
 //! Entrypoint.
+use std::error::Error;
 
 use inserter::ClickhouseClient;
 
-fn main() {
+use tokio::runtime::Runtime;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let rt = Runtime::new()?;
+    rt.block_on(async_main())
+}
+
+async fn async_main() -> Result<(), Box<dyn Error>> {
+    println!("Initializing ClickHouse client...");
+    let clickhouse_client = ClickhouseClient::new("http://localhost:8123")?;
+    clickhouse_client.init_schema().await?;
+
+    println!("Initializing extractor...");
     extractor::extractor();
-    ClickhouseClient::new("http://localhost:8123");
+
+    Ok(())
 }
