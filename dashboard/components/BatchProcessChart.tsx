c44c8d9029a708446095223e44575ddb2715decf
Håvard Anda Estensen <haavard.ae@gmail.com>
2025-06-02 12:32:00 +0200

    chore: less chart spacing

--- dashboard/components/BatchProcessChart.tsx
+++ dashboard/components/BatchProcessChart.tsx
@@ -69,7 +69,7 @@ export const BatchProcessChart: React.FC<BatchProcessChartProps> = ({
     <ResponsiveContainer width="100%" height="100%">
       <LineChart
         data={data}
-        margin={{ top: 5, right: 30, left: 20, bottom: 60 }}
+        margin={{ top: 5, right: 30, left: 20, bottom: 40 }}
       >
         <CartesianGrid strokeDasharray="3 3" stroke="#e0e0e0" />
         <XAxis
--- dashboard/components/TpsChart.tsx
+++ dashboard/components/TpsChart.tsx
@@ -31,7 +31,7 @@ export const TpsChart: React.FC<TpsChartProps> = ({ data, lineColor }) => {
     <ResponsiveContainer width="100%" height="100%">
       <LineChart
         data={data}
-        margin={{ top: 5, right: 30, left: 20, bottom: 50 }}
+        margin={{ top: 5, right: 30, left: 20, bottom: 40 }}
       >
         <CartesianGrid strokeDasharray="3 3" stroke="#e0e0e0" />
         <XAxis
