a874943d80b6f7a6c3740f6d9c3190f45c67f30d
Håvard Anda Estensen <haavard.ae@gmail.com>
2025-06-03 10:19:13 +0200

    chore(dashboard): remove unused API_KEY env

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,12 +1,8 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+export default defineConfig(() => {
   return {
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY),
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
