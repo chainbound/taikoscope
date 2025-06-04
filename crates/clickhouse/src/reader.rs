7e21db561fdaa6ebcc1f8d1456c61b20b7e6adf9
Håvard Anda Estensen <haavard.ae@gmail.com>
2025-06-04 13:59:18 +0200

    fix: base fee calc

--- crates/clickhouse/src/reader.rs
+++ crates/clickhouse/src/reader.rs
@@ -1536,7 +1536,7 @@ impl ClickhouseReader {
         }
 
         let mut query = format!(
-            "SELECT sum(sum_priority_fee + sum_base_fee * 3 / 4) AS total \
+            "SELECT sum(sum_priority_fee + toUInt128(sum_base_fee * 3 / 4)) AS total \
              FROM {db}.l2_head_events \
              WHERE block_ts >= toUnixTimestamp(now64() - INTERVAL {interval})",
             interval = range.interval(),
