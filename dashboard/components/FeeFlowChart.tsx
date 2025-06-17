90d2f4deb11c533980759b8ead1ddb756ebcfdfe
Håvard Anda Estensen <haavard.ae@gmail.com>
2025-06-17 17:26:40 +0200

    fix(dashboard): sort Sankey nodes

--- dashboard/components/FeeFlowChart.tsx
+++ dashboard/components/FeeFlowChart.tsx
@@ -103,7 +103,7 @@ export const FeeFlowChart: React.FC<FeeFlowChartProps> = ({
           nodePadding={10}
           node={{ stroke: '#888' }}
           link={renderLink}
-          sort={false}
+          sort={true}
         >
           <Tooltip
             formatter={(v: number) => `$${v.toFixed(2)}`}
