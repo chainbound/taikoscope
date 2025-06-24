4bf9547c4d7358190b245052fdda20ba958e6556
Håvard Anda Estensen <haavard.ae@gmail.com>
2025-06-24 17:22:01 +0200

    chore: add hekla mappings

--- dashboard/sequencerConfig.ts
+++ dashboard/sequencerConfig.ts
@@ -1,6 +1,9 @@
 export const SEQUENCER_PAIRS: Array<[string, string]> = [
   ['0x00a00800c28f2616360dcfadee02d761d14ad94e', 'Chainbound A'],
   ['0x00b00194cdc219921784ab1eb4eaa9634fe1f1a8', 'Chainbound B'],
+  ['0x205a600d515091b473b6c1a8477d967533d10749', 'Chainbound'],
+  ['0x445179507c3b0b84cca739398966236a35ad8ea1', 'Gattaca'],
+  ['0x75141cd01f50a17a915d59d245ae6b2c947d37d9', 'Nethermind'],
 ];
 
 export const SEQUENCER_NAME_BY_ADDRESS: Record<string, string> =
